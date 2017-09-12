c9cc84e35e55195ed151ff8ebbcb48f44a8722a9
ASHIKA <ASHIKA@ASHIKA-PC>
2017-09-12 00:15:50 -0500

    This is first commit

---
+++ Gitabc1/src/package1/SimpleJavaCode.java
@@ -0,0 +1,16 @@
+package package1;
+
+public class SimpleJavaCode {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=10;i++){
+			if(i==10){
+				break;
+			}
+			System.out.println(i);
+		
+		}
+
+	}
+
+}
